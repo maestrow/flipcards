257808d74f358deb68d100645829cbadd3f1dc6a
maestrow <andrew.shmelev@gmail.com>
2021-08-02 23:19:11 +0300

    csrf allow

--- django/src/fcards/views.py
+++ django/src/fcards/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
+
+@csrf_exempt
+def sync(request):
+  text = request.body.decode('utf8')
+  data = json.loads(text)
+  return JsonResponse({'foo': data})
+
+
+def vote(request, question_id):
+   return HttpResponse("You're voting on question %s." % question_id)
