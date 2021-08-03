556dddbbe06554041765e2906bc9029149a49c7c
maestrow <andrew.shmelev@gmail.com>
2021-08-03 17:07:14 +0300

    - build front with make. It's easier because I can use shell commands, to copy files with globs, for instance; - extension building task;

--- front/scripts/build.js
+++ front/scripts/build.js
@@ -1,16 +1,5 @@
 const options = require('./esbuild-config.js');
 const { build } = require('esbuild');
-const fs = require('fs');
-const path = require('path');
-const { exit } = require('process');
-
-const copy = () => {
-  const src = path.resolve(__dirname, '../public/index.html')
-  const dest = path.resolve(__dirname, '../dist/index.html')
-  fs.copyFileSync(src, dest);
-}
-
-copy();
 
 build(options).catch(err => {
   process.stderr.write(err.stderr)
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -2,4 +2,15 @@ import React, { useCallback, useState } from 'react';
 import ReactDOM from "react-dom";
 import { App } from './components/app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function htmlToElement(html: string): HTMLElement {
+  var template = document.createElement('template');
+  html = html.trim(); // Never return a text node of whitespace as the result
+  template.innerHTML = html;
+  return template.content.firstChild as HTMLElement;
+}
+
+const id = 'vocab-k09asnd';
+
+document.body.appendChild(htmlToElement(`<div id='${id}'></div>`));
+
+ReactDOM.render(<App />, document.getElementById(id));
